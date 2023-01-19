71d8c930b6de7a002369e5c031df22e9cc0a7906
GitHub Action <action@github.com>
2023-01-19 00:38:34 +0000

    Publishing example examples/spartacus

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Edgio Applications Spartacus Example
+# edgioSpartacusExample
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.1.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
       backend: {
         occ: {
           baseUrl:
-            'https://edgio-community-layer0-spartacus-example-default.layer0-limelight.link',
+            'https://edgio-community-examples-spartacus-live.layer0-limelight.link',
         },
       },
       context: {
